71ed561a23e44feaeda22494cfeff393d82c908e
Lucas Tolly <ltolly@madisoncollege.edu>
2017-02-02 13:22:18 -0600

    Adding affinity model

---
+++ Vagante Scholars/src/Affinity.java
@@ -0,0 +1,39 @@
+/**
+ * Lucas Tolly
+ * 2/2/2017
+ * This class is a model for the Affinity table.
+ */
+public class Affinity {
+
+    private int affinityId;
+    private int classId;
+    private int affinityName;
+
+    public Affinity {
+
+    }
+
+    /**
+     * This is a getter for an affinity's id
+     * @return The affinity's id
+     */
+    public int getAffinityId() {
+        return affinityId;
+    }
+
+    /**
+     * This is a getter for an affinity's class id
+     * @return
+     */
+    public int getClassId() {
+        return classId;
+    }
+
+    /**
+     * This is a getter for an affinity's name
+     * @return
+     */
+    public int getAffinityName() {
+        return affinityName;
+    }
+}
