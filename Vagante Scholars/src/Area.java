d9f64729752da5672bd7e09cbda8c28436ae3f5e
Lucas Tolly <ltolly@madisoncollege.edu>
2017-02-02 13:37:28 -0600

    Added the Area class

---
+++ Vagante Scholars/src/Area.java
@@ -0,0 +1,31 @@
+/**
+ * This class is the model for the Area table.
+ */
+public class Area {
+
+    private int areaId;
+    private int description;
+
+    /**
+     * Empty constructor currently
+     */
+    public Area {
+
+    }
+
+    /**
+     * This is the getter for the Area's id
+     * @return the Area's id
+     */
+    public int getAreaId() {
+        return areaId;
+    }
+
+    /**
+     * This is the getter for the Area's description
+     * @return the Area's description
+     */
+    public int getDescription() {
+        return description;
+    }
+}
