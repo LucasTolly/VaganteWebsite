46f3612e9b680a498a11825d6052bafb209d25da
Lucas Tolly <ltolly@madisoncollege.edu>
2017-02-02 13:29:18 -0600

    Adding CharacterClass class

---
+++ Vagante Scholars/src/CharacterClass.java
@@ -0,0 +1,76 @@
+/**
+ * Lucas Tolly
+ * 2/2/2017
+ * This class is the model for the Class Table (referenced as
+ * CharacterClass to avoid reserved word issues).
+ */
+public class CharacterClass {
+
+    private int classId;
+    private String className;
+    private int startingStrength;
+    private int startingDexterity;
+    private int startingVitality;
+    private int startingIntelligence;
+    private int startingLuck;
+
+    public CharacterClass() {
+        
+    }
+
+    /**
+     * This is the getter for a class' id
+     * @return the class' id
+     */
+    public int getClassId() {
+        return classId;
+    }
+
+    /**
+     * This is the getter for a class' name
+     * @return the class' name
+     */
+    public String getClassName() {
+        return className;
+    }
+
+    /**
+     * This is the getter for a class' starting strength
+     * @return the class' starting strength
+     */
+    public int getStartingStrength() {
+        return startingStrength;
+    }
+
+    /**
+     * This is the getter for a class' starting dexterity
+     * @return the class' starting dexterity
+     */
+    public int getStartingDexterity() {
+        return startingDexterity;
+    }
+
+    /**
+     * This is the getter for a class' starting vitality
+     * @return the class' starting vitality
+     */
+    public int getStartingVitality() {
+        return startingVitality;
+    }
+
+    /**
+     * This is the getter for a class' starting intelligence
+     * @return the class' starting intelligence
+     */
+    public int getStartingIntelligence() {
+        return startingIntelligence;
+    }
+
+    /**
+     * This is the getter for a class' starting luck
+     * @return the class' starting luck
+     */
+    public int getStartingLuck() {
+        return startingLuck;
+    }
+}
